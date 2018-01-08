98d5738f699a0543642d48db99d78488dcf00d45
Bo Eun Lee <bolee417@gmail.com>
2018-01-08 19:21:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers' , 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (array, element) {
-  [element, ... array];
-  var newArr = array;
+  var newArr = [element, ... array];
   return newArr;
 }
 
